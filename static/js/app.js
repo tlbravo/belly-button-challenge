8e577d95fc50895d21eca5c3b9a96aebfb78fed0
tlbravo <tbravo507@gmail.com>
2023-10-06 12:15:07 -0400

    start bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -25,17 +25,62 @@ function getData() {
         // first name in the list
         let first_sample = Names [0];
         // Initialize plots
-        sampleMetadata(first_sample);
-        sampleBarChart(first_sample);
-        sampleBubblechart(first_sample);
+        sampleMetadatametadata(first_sample);
+        BarChart(first_sample);
+        Bubblechart(first_sample);
     }); 
 
   };
+//Itterate through metadata in samples
 
-// let values = sample_values
+function Metadata(samples) {
+    // Retrieve JSON data and use console log
+    d3.json(url).then((data) => {
 
-// let trace1 = {
-//     x: books,
-//     y: timesRead,
-//     type: 'bar'
-//   };
+        //Declare metadata variable
+        let metadatavar = data.metadatavar;
+        // Filter metadata then log results
+        let info = metadatavar.filter(result => result.id == samples);
+            console.log(info)
+
+        //Retrive first value
+        let first_data = info[0];
+        //Clear metadata. Use "sample-metadata" name from index.html
+        d3.select("#sample-metadata").html("");
+
+        //Add key value pair. https://www.geeksforgeeks.org/javascript-object-entries-method/
+        let entries = Object.entries(obj);
+        // Append to metadata and log
+        entries.forEach(([key,value])=> {d3.select("#sample-metadata").append("h5").text(`{$key}:{$value}`); });
+        console.log(entries);
+    }); 
+
+}
+//Create a horizontal bar chart
+function BarChart(samples) {
+   
+    // Retrieve JSON data and use console log
+    d3.json(url).then((data) => {
+        console.log(`Data: ${data}`);
+        
+        //Sample array
+        let barSample = data.samples;
+        
+        //Filter sample array
+        let filteredSample = samples.filter((barSample) => barSample.id == samples);
+       
+        //First index 
+        let firstFilteredSample = filteredSample[0];
+        
+        //set ids, lables, values 
+        let otu_ids = firstFilteredSample.otu_ids;
+        let otu_labels = firstFilteredSample.otu_labels;
+        let sample_values = firstFilteredSample.sample_values;
+        
+        //Console log above 
+        console.log(otu_ids, otu_labels, sample_values);
+
+        //Display top 10 OTUs
+
+
+}
